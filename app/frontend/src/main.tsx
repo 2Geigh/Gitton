80bcedb2b748e0b731ab6b06116334c987f34872
Nick Garcia <nicholasgarcia2003@protonmail.com>
2026-03-17 14:49:56 -0600

    npm install @fontsource-variable/open-sans

--- app/frontend/src/main.tsx
+++ app/frontend/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles/main.scss';
+import '@fontsource-variable/open-sans';
 
 const container = document.getElementById('root');
 
